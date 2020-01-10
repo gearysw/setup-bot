3b976a039c483ab4006b011bccbd4479afc1db57
gearysw <46032052+gearysw@users.noreply.github.com>
2020-01-09 23:32:52 -0800

    fixed drinks tracking and added private setup search

--- cmds/drink.js
+++ cmds/drink.js
@@ -6,8 +6,8 @@ module.exports = {
     aliases: ['soda', 'sodas'],
     description: 'Keep track of drinks intake and leaderboard',
     args: false,
-    easteregg: true,
-    usage: '<number> | leaderboard',
+    easteregg: false,
+    usage: 'number || leaderboard',
     execute: async (bot, message, args) => {
         if (!args.length) {
             fs.readFile('./drinks.json', (err, data) => {
@@ -29,7 +29,7 @@ module.exports = {
                     fs.writeFile('./drinks.json', JSON.stringify(json), err => {
                         if (err) return console.error(err);
                         message.channel.send(`${NAME} drowns his sorrows with a drink. He's now tracked 1 drink.`);
-                    });
+                    }).catch(console.error);
                 } else {
                     let Drinks = json[objIndex].drinks + 1;
 
@@ -47,96 +47,140 @@ module.exports = {
                     fs.writeFile('./drinks.json', JSON.stringify(updatedDrinks), err => {
                         if (err) return console.error(err);
                         message.channel.send(`${NAME} drowns his sorrows with a drink. He's now tracked ${Drinks} drinks.`);
-                    });
+                    }).catch(console.error);
                 }
-            });
+            }).catch(console.error);
         }
-        if (args[0] && !isNaN(args[0])) {
-            fs.readFile('./drinks.json', (err, data) => {
-                if (err) return console.error(err);
-                let json = JSON.parse(data);
-
-                const ID = message.author.id;
-                const NAME = (!message.member.nickname) ? message.author.username : message.member.nickname;
-                const objIndex = json.findIndex(obj => obj.id === ID);
-
-                if (objIndex === -1) {
-                    const Drinks = args[0];
-                    let reply;
-
-                    if (args[0] <= 0) return message.channel.send(`You can't do that!`);
-                    if (args[0] === '1') {
-                        reply = `${NAME} drowns his sorrows with a drink. He's now tracked 1 drink.`;
-                    } else if (args[0] > 1) {
-                        reply = `${NAME} drowns his sorrows with ${args[0]} drinks. He's now tracked ${args[0]} drinks.`;
-                    }
-
-                    const trackDrink = {
-                        "id": ID,
-                        "name": NAME,
-                        "drinks": parseInt(Drinks)
-                    };
-
-                    json.push(trackDrink);
-                    fs.writeFile('./drinks.json', JSON.stringify(json), err => {
+        if (args[0]) {
+            if (isNaN(args[0])) {
+                if (args[0] === 'leaderboard') {
+                    fs.readFile('./drinks.json', (err, data) => {
                         if (err) return console.error(err);
-                        message.channel.send(reply);
-                    });
+                        const json = JSON.parse(data);
+                        if (!json.length) return message.channel.send(`Nobody's tracked their drinks yet. Drink up!`);
+
+                        const sorted = json.slice(0).sort((a, b) => {
+                            return b.drinks - a.drinks;
+                        });
+
+                        let leaderboard = '';
+                        for (const d of sorted) {
+                            leaderboard = leaderboard + `${d.drinks} - ${d.name}\n`;
+                        }
+                        const totalDrinks = sorted.map(s => s.drinks).reduce((a, b) => a + b);
+
+                        const leaderboardEmbed = new Discord.RichEmbed()
+                            .setTitle('Drinks Leaderboard')
+                            .setDescription(leaderboard)
+                            .setColor('#FF5555')
+                            .addField('Total Drinks', `CARL has collectively drowned their sorrows with ${totalDrinks} drinks.`);
+
+                        message.channel.send(leaderboardEmbed);
+                    }).catch(console.error);
                 } else {
-                    let Drinks = json[objIndex].drinks;
-                    let reply;
-
-                    if (args[0] <= 0) return message.channel.send(`You can't do that!`);
-                    if (args[0] === '1') {
-                        Drinks = Drinks + 1;
-                        reply = `${NAME} drowns his sorrows with a drink. He's now tracked ${Drinks} drinks.`;
-                    } else if (args[0] > 1) {
-                        Drinks = Drinks + parseInt(args[0]);
-                        reply = `${NAME} drowns his sorrows with ${args[0]} drinks. He's now tracked ${Drinks} drinks.`;
-                    }
-
-                    const trackDrink = {
-                        "id": ID,
-                        "name": NAME,
-                        "drinks": Drinks
-                    };
-                    const updatedDrinks = [
-                        ...json.slice(0, objIndex),
-                        trackDrink,
-                        ...json.slice(objIndex + 1),
-                    ];
-
-                    fs.writeFile('./drinks.json', JSON.stringify(updatedDrinks), err => {
+                    fs.readFile('./drinks.json', (err, data) => {
                         if (err) return console.error(err);
-                        message.channel.send(reply);
-                    });
+                        let json = JSON.parse(data);
+
+                        const ID = message.author.id;
+                        const NAME = (!message.member.nickname) ? message.author.username : message.member.nickname;
+                        const objIndex = json.findIndex(obj => obj.id === ID);
+
+                        if (objIndex === -1) {
+                            const trackDrink = {
+                                "id": ID,
+                                "name": NAME,
+                                "drinks": 1
+                            };
+
+                            json.push(trackDrink);
+                            fs.writeFile('./drinks.json', JSON.stringify(json), err => {
+                                if (err) return console.error(err);
+                                message.channel.send(`${NAME} drowns his sorrows with a drink. He's now tracked 1 drink.`);
+                            }).catch(console.error);
+                        } else {
+                            let Drinks = json[objIndex].drinks + 1;
+
+                            const trackDrink = {
+                                "id": ID,
+                                "name": NAME,
+                                "drinks": parseInt(Drinks)
+                            };
+                            const updatedDrinks = [
+                                ...json.slice(0, objIndex),
+                                trackDrink,
+                                ...json.slice(objIndex + 1),
+                            ];
+
+                            fs.writeFile('./drinks.json', JSON.stringify(updatedDrinks), err => {
+                                if (err) return console.error(err);
+                                message.channel.send(`${NAME} drowns his sorrows with a drink. He's now tracked ${Drinks} drinks.`);
+                            }).catch(console.error);
+                        }
+                    }).catch(console.error);
                 }
-            });
-        }
-        if (args[0] && args[0] === 'leaderboard') {
-            fs.readFile('./drinks.json', (err, data) => {
-                if (err) return console.error(err);
-                const json = JSON.parse(data);
-                if (!json.length) return message.channel.send(`Nobody's tracked their drinks yet. Drink up!`);
+            } else if (!isNaN(args[0])) {
+                fs.readFile('./drinks.json', (err, data) => {
+                    if (err) return console.error(err);
+                    let json = JSON.parse(data);
 
-                const sorted = json.slice(0).sort((a, b) => {
-                    return b.drinks - a.drinks;
-                });
+                    const ID = message.author.id;
+                    const NAME = (!message.member.nickname) ? message.author.username : message.member.nickname;
+                    const objIndex = json.findIndex(obj => obj.id === ID);
 
-                let leaderboard = '';
-                for (const d of sorted) {
-                    leaderboard = leaderboard + `${d.drinks} - ${d.name}\n`;
-                }
-                const totalDrinks = sorted.map(s => s.drinks).reduce((a, b) => a + b);
+                    if (objIndex === -1) {
+                        const Drinks = args[0];
+                        let reply;
 
-                const leaderboardEmbed = new Discord.RichEmbed()
-                    .setTitle('Drinks Leaderboard')
-                    .setDescription(leaderboard)
-                    .setColor('#FF5555')
-                    .addField('Total Drinks', `CARL has collectively drowned their sorrows with ${totalDrinks} drinks.`);
+                        if (args[0] <= 0) return message.channel.send(`You can't do that!`);
+                        if (args[0] === '1') {
+                            reply = `${NAME} drowns his sorrows with a drink. He's now tracked 1 drink.`;
+                        } else if (args[0] > 1) {
+                            reply = `${NAME} drowns his sorrows with ${args[0]} drinks. He's now tracked ${args[0]} drinks.`;
+                        }
 
-                message.channel.send(leaderboardEmbed);
-            });
+                        const trackDrink = {
+                            "id": ID,
+                            "name": NAME,
+                            "drinks": parseInt(Drinks)
+                        };
+
+                        json.push(trackDrink);
+                        fs.writeFile('./drinks.json', JSON.stringify(json), err => {
+                            if (err) return console.error(err);
+                            message.channel.send(reply);
+                        }).catch(console.error);
+                    } else {
+                        let Drinks = json[objIndex].drinks;
+                        let reply;
+
+                        if (args[0] <= 0) return message.channel.send(`You can't do that!`);
+                        if (args[0] === '1') {
+                            Drinks = Drinks + 1;
+                            reply = `${NAME} drowns his sorrows with a drink. He's now tracked ${Drinks} drinks.`;
+                        } else if (args[0] > 1) {
+                            Drinks = Drinks + parseInt(args[0]);
+                            reply = `${NAME} drowns his sorrows with ${args[0]} drinks. He's now tracked ${Drinks} drinks.`;
+                        }
+
+                        const trackDrink = {
+                            "id": ID,
+                            "name": NAME,
+                            "drinks": Drinks
+                        };
+                        const updatedDrinks = [
+                            ...json.slice(0, objIndex),
+                            trackDrink,
+                            ...json.slice(objIndex + 1),
+                        ];
+
+                        fs.writeFile('./drinks.json', JSON.stringify(updatedDrinks), err => {
+                            if (err) return console.error(err);
+                            message.channel.send(reply);
+                        }).catch(console.error);
+                    }
+                }).catch(console.error);
+            }
         }
     }
 }
--- cmds/teamsearch.js
+++ cmds/teamsearch.js
@@ -2,18 +2,26 @@ const Discord = require('discord.js');
 const config = require('../config.json');
 
 module.exports = {
-    name: 'search',
-    aliases: ['searchfiles', 'filesearch', 'files'],
-    description: 'Searches through the repository for files with the search term(s)',
+    name: 'teamsearch',
+    aliases: ['secret', 'carldok', 'teamsetup', 'sekrit'],
+    description: 'Searches through the team role repository for files with the search term(s)',
     args: false,
-    easteregg: false,
+    easteregg: true,
     usage: '<search terms>',
     execute: async (bot, message, args) => {
+        const teams = ['Team 1', 'Team 2', 'Team 3', 'Team 4', 'Team 5'];
+        const channels = ['teams-overall-chat', 'team-1', 'team-2', 'team-3', 'team-4', 'team-5'];
+        const memberRoles = message.member.roles.map(r => r.name);
+        const eligible = teams.some(t => memberRoles.includes(t));
+
+        if (!eligible) return;
+        if (!channels.some(c => c.includes(message.channel.name))) return;
+
         if (!args.length) {
             const helpEmbed = new Discord.RichEmbed()
                 .setColor('#ff5555')
                 .setTitle('Searching for setups')
-                .setDescription(`Search for setups by typing \`${config.prefix}search <terms>\`\nDiscord only supports up to 25 embed fields, so only the latest 25 setups is shown.`);
+                .setDescription(`Search for setups by typing \`${config.prefix}teamsearch <terms>\`\nDiscord only supports up to 25 embed fields, so only the latest 25 setups is shown.`);
 
             return message.channel.send(helpEmbed);
         }
@@ -21,23 +29,23 @@ module.exports = {
         const searching = await message.channel.send('Searching...');
         let msgAttachments = [];
         let msgUrl = [];
-        bot.guilds.get(config.CARLServer).channels.get(config.Repository).fetchMessages({
+        bot.guilds.get(config.CARLServer).channels.get(config.PrivateRepo).fetchMessages({
             limit: 100
-        }).then(fetched1 => bot.guilds.get(config.CARLServer).channels.get(config.Repository).fetchMessages({
+        }).then(fetched1 => bot.guilds.get(config.CARLServer).channels.get(config.PrivateRepo).fetchMessages({
             limit: 100,
             before: fetched1.last().id
-        })).then(fetched2 => bot.guilds.get(config.CARLServer).channels.get(config.Repository).fetchMessages({
+        })).then(fetched2 => bot.guilds.get(config.CARLServer).channels.get(config.PrivateRepo).fetchMessages({
             limit: 100,
             before: fetched2.last().id
-        })).then(fetched3 => bot.guilds.get(config.CARLServer).channels.get(config.Repository).fetchMessages({
+        })).then(fetched3 => bot.guilds.get(config.CARLServer).channels.get(config.PrivateRepo).fetchMessages({
             limit: 100,
             before: fetched3.last().id
-        })).then(fetched4 => bot.guilds.get(config.CARLServer).channels.get(config.Repository).fetchMessages({
+        })).then(fetched4 => bot.guilds.get(config.CARLServer).channels.get(config.PrivateRepo).fetchMessages({
             limit: 100,
             before: fetched4.last().id
         })).catch(console.error);
 
-        message.guild.channels.get(config.Repository).messages.map(a => {
+        message.guild.channels.get(config.PrivateRepo).messages.map(a => {
             if (a.attachments.size) {
                 msgAttachments.push(a.attachments.first().filename);
                 msgUrl.push(a.url);
